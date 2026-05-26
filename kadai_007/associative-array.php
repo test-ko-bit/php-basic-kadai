f56d4c982174fe67640b0bb5cc229d90480befdb
test-ko-bit <test.sql.0110@gmail.com>
2026-05-26 13:33:29 +0900

    kadai_007作成

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,12 @@
+
+<body>
+    <p>
+        <?php
+        // 連想配列を作成
+        $list = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+        print_r($list);
+        ?>
+    </p>
+</body>
+
+</html>
